0cc9a401c968901f07414461fb7606ff37713bd3
Tofa Hassan <hasan.tofa@gmail.com>
2024-08-06 22:34:19 +0600

    rapid api key placed in env file for security

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   baseURL: 'https://imdb188.p.rapidapi.com/api/v1', // Replace with your API base URL
   timeout: 100000,
   headers: {
-    'x-rapidapi-key': '4c3c792974mshaf485277d9eb8dbp1379eejsncbb054025214',
+    'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
     'x-rapidapi-host': 'imdb188.p.rapidapi.com',
     'Content-Type': 'application/json'
   },
